c415bebed161508fac83d610bf3580e1ce44a72d
AnneRoki <59899443+AnneRoki@users.noreply.github.com>
2022-02-04 12:29:32 +0800

    wew

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getjoke(){
-    murl ="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&type=single"
+    murl ="https://v2.jokeapi.dev/joke/Dark?type=single"
     fetch(murl)
     .then(function(response){
         console.log(response)
@@ -15,4 +15,44 @@ function getjoke(){
 
 function display_joke(wew){
     document.getElementById("joke").innerHTML =wew
+}
+
+
+function getjoke1(){
+    murl ="https://v2.jokeapi.dev/joke/Pun?type=single"
+    fetch(murl)
+    .then(function(response){
+        console.log(response)
+        return response.json();
+    })
+    .then(function(data){
+        display_joke1(data.joke)
+    })
+    .catch(function(error){
+        console.log("Error: " + error);
+    });
+}
+
+function display_joke1(wew){
+    document.getElementById("joke1").innerHTML =wew
+}
+
+
+function getjoke2(){
+    murl ="https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw&type=single"
+    fetch(murl)
+    .then(function(response){
+        console.log(response)
+        return response.json();
+    })
+    .then(function(data){
+        display_joke2(data.joke)
+    })
+    .catch(function(error){
+        console.log("Error: " + error);
+    });
+}
+
+function display_joke2(wew){
+    document.getElementById("joke2").innerHTML =wew
 }
